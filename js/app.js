42145f29c84d1bb756906438d90ee1bc6baf197c
Dan Kempton <kemptondan@gmail.com>
2018-04-24 17:16:21 -0400

    initial commit html/js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Cities JSON data to pull from
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+// Should have array to put cities in
+const cities = [];
