04cf43812787e29a4949f528b5d4926dc14cca97
Dan Kempton <kemptondan@gmail.com>
2018-04-24 18:38:01 -0400

    added css styles

--- js/app.js
+++ js/app.js
@@ -3,3 +3,19 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 
 // Should have array to put cities in
 const cities = [];
+
+// Should fetch promise
+fetch(endpoint).then(blob => blob.json()).then(data => cities.push(...data)); // spreading into the push method
+// Should filter array down to a subset for searching the cities
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        // Should figure out if city or state matches what was searched
+        const regex = new RegExp(wordToMatch, 'gi');  // g = global, match all instances in a string, not just one, i= case-sensitive
+        return place.city.match(regex) || place.state.match(regex);  // returns either the city or the state
+    });
+}
+
+// Include commas in population numbers
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
